af66f64def8215ed93102df7928d188d127cc190
alexo75 <alexander.oneill75@gmail.com>
2023-02-12 16:30:58 -0500

    commiting free-memory changes

--- helloworld.java
+++ helloworld.java
@@ -1,10 +1,15 @@
+package main;
 
 public class helloworld {
 	
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+        Runtime memCalc = Runtime.getRuntime();
 		System.out.println("Hear ye, hear ye: important annnouncement");
+
+        System.out.println("Total memory is: " + memCalc.totalMemory());
+        System.out.println("Free memory is: " + memCalc.freeMemory());
 		System.out.println("Hello Worlde");
 		System.out.println("Hello Alice!");
 		System.out.println("Hey Bob!");
@@ -13,7 +18,6 @@ public class helloworld {
 		System.out.println("Bonjour Ester!");
 		System.out.println("Fare thee well!");
 		
-
 
 
 	}
