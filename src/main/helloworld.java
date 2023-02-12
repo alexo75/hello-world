bb145b592aeb41e5ea341b6896bdc26ab0259491
alexo75 <alexander.oneill75@gmail.com>
2023-02-12 18:07:51 -0500

    Now displays date as well as time

--- src/main/helloworld.java
+++ src/main/helloworld.java
@@ -1,13 +1,16 @@
 package main;
 
+import java.util.Date;
+
 public class helloworld {
 	
 	
-
+//	public class GFG {
+	
 	public static void main(String[] args) {	
         Runtime memCalc = Runtime.getRuntime();
-		System.out.println("Hear ye, hear ye: important annnouncement");
-
+        getDate();
+		
         System.out.println("Total memory is: " + memCalc.totalMemory());
         System.out.println("Free memory is: " + memCalc.freeMemory());
 		System.out.println("Hello Worlde");
@@ -17,8 +20,17 @@ public class helloworld {
 		System.out.println("Howdy Denise!");
 		System.out.println("Bonjour Ester!");
 		System.out.println("Fare thee well!");
-		
 
 
+	}
+
+private static void getDate() {
+		  {
+		        // Creating object of date class
+		        Date dateAndTime = new Date();
+		 
+		        // Printing the value stored in above object
+		        System.out.println("Current date and time is " + dateAndTime);
+		  }
 	}
 }
